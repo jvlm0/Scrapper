7def314cf0d98750b0570216f5032360c20b5089
jonorex <jonofex@gmail.com>
2025-10-28 16:57:08 -0300

    Main da aplicação

---
+++ src/main/java/com/scraping/vagas/Scraping/ScrapingApplication.java
@@ -0,0 +1,15 @@
+package com.scraping.vagas.Scraping;
+
+import com.scraping.vagas.Scraping.model.SiteModel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapingApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(ScrapingApplication.class, args);
+	}
+
+}
